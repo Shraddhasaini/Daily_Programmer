9b674bc267f0d6dd6d44381ed83d2c2d7fe55ed7
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-14 16:40:22 +0530

    ADDED DICT

--- sortdictbyvalue.py
+++ sortdictbyvalue.py
@@ -1,1 +1,10 @@
 #Write a Python script to sort (ascending and descending) a dictionary by value.
+mydict = {
+            'key1' : 1,
+            'key11' : 11,
+            'key111' : 111,
+            'key1111' : 1111,
+            'key11111' : 11111,
+            'key111111' : 11111,
+            'key1111111' : 11111,
+}
