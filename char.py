407ad1ac6c99e24a01604d5e2a0aefd61a957f50
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-14 17:16:31 +0530

    problem statement

---
+++ char.py
@@ -0,0 +1,4 @@
+#Write a Python program to get a string from a given string
+#where all occurrences of its first char have been changed to
+#'$', except the first char itself. Write a Python program that
+#matches a string that has an 'a' followed by anything, ending in 'b.
