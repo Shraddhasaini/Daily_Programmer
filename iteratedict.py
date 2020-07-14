e30cc5f8ddb61cbc41d934d31a79a640d2619764
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-14 17:07:36 +0530

    added dict to iterate

---
+++ iteratedict.py
@@ -0,0 +1,20 @@
+#Write a Python program to iterate over dictionaries using for loops.
+mydict0 = {
+            'key0000000' : 0,
+            'key000000' : 00,
+            'key00000' : 000,
+            'key0000' : 0000,
+            'key000' : 00000,
+            'key00' : 000000,
+            'key0' : 0000000,
+}
+
+mydict = {
+            'key1111111' : 1,
+            'key111111' : 11,
+            'key11111' : 111,
+            'key1111' : 1111,
+            'key111' : 11111,
+            'key11' : 111111,
+            'key1' : 1111111,
+}
