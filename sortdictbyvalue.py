4f48d530a86a7dfe95ceb09e378c9c53c4d5c002
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-14 16:53:13 +0530

    dict

--- sortdictbyvalue.py
+++ sortdictbyvalue.py
@@ -1,13 +1,13 @@
 #Write a Python script to sort (ascending and descending) a dictionary by value.
 mydict = {
-            'key1' : 1,
-            'key11' : 11,
-            'key111' : 111,
+            'key1111111' : 1,
+            'key111111' : 11,
+            'key11111' : 111,
             'key1111' : 1111,
-            'key11111' : 11111,
-            'key111111' : 111111,
-            'key1111111' : 1111111,
+            'key111' : 11111,
+            'key11' : 111111,
+            'key1' : 1111111,
 }
 
-for x in mydict.values():
-    print(x)
+print('ascending: '+ str(sorted(mydict.values())))
+#print(sorted(mydict.values()(reverse='true'))
