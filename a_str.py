fdc272b48af0776e9141b5da7627915ca417be31
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-16 09:59:21 +0530

    one line

--- a_str.py
+++ a_str.py
@@ -1,4 +1,3 @@
 #Write a Python program that matches a string that has an 'a' followed by anything, ending in 'b.
 import re
-if re.search('a.*?b$',  'a end with b'):
-    print('match')
+if re.search('a.*?b$',  'a end with b'): print('match')
