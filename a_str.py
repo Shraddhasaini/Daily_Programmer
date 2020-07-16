3e68a5f1c5f106d3c7a0d886658bf5525074bf1e
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-16 09:56:42 +0530

    solution

--- a_str.py
+++ a_str.py
@@ -1,1 +1,7 @@
 #Write a Python program that matches a string that has an 'a' followed by anything, ending in 'b.
+import re
+str0 = 'a end with b'
+str1 = 'not satisfying string'
+
+if re.search('a.*?b$',  str0):
+    print(match)
