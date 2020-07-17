5cb2a3f65bd05d0dd74485c85956efe4da279e81
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-17 11:16:59 +0530

    decorator

--- decorator.py
+++ decorator.py
@@ -1,1 +1,22 @@
 #Write a Python program to make a chain of function decorators (bold, italic, underline etc.) in Python.
+def bold(x):
+    def func():
+        return "<b>" + x() + "</b>"
+    return func
+
+def italic(x):
+    def func():
+        return "<i>" + x() + "</i>"
+    return func
+
+def underline(x):
+    def func():
+        return "<u>" + x() + "</u>"
+    return func
+
+@bold
+@italic
+@underline
+def functoprint():
+    return "Hello world"
+print(functoprint())
