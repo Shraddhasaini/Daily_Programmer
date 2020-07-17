1b80e54680fe2f12b0297a799254b38c60abaa13
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-17 11:00:31 +0530

    iterating over the list

--- literals.py
+++ literals.py
@@ -1,3 +1,5 @@
 #Write a Python program to search some literals strings in a string.
 literals = ['literals','strings','integer']
 str = 'This sentence contains literals strings'
+for i in literals:
+    print(i)
